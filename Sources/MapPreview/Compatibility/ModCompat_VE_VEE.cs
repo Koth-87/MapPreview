38a111024ae015c8cf31e817bfea18677e9ad0b6
m00nl1ght <github@m00nl1ght.dev>
2026-08-01 12:52:27 +0200

    Make VEE compat patch conditional so it only applies to old versions of VEE

--- Sources/MapPreview/Compatibility/ModCompat_VE_VEE.cs
+++ Sources/MapPreview/Compatibility/ModCompat_VE_VEE.cs
@@ -9,6 +9,13 @@ internal class ModCompat_VE_VEE : ModCompat
     public override string TargetAssemblyName => "VEE";
     public override string DisplayName => "Vanilla Events Expanded";
 
+    protected override bool OnApply()
+    {
+        // This patch is no longer needed in VEE v2.x
+        // Therefore it's conditional now and only applies to old versions of VEE
+        return AccessTools.Method("VEE.FertilityGrid_Patch:Postfix") != null;
+    }
+
     [HarmonyPrefix]
     [HarmonyPatch("VEE.FertilityGrid_Patch", "Postfix")]
     private static bool FertilityGrid_Patch()
